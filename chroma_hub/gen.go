598d846d8d8ea69f99af93da01b46cd3eff9a463
Josh <joshua.childs01@outlook.com>
2024-03-07 19:03:00 +1100

    updated gen hub function

--- chroma_hub/gen.go
+++ chroma_hub/gen.go
@@ -2,14 +2,14 @@ package chroma_hub
 
 import "github.com/jchilds0/chroma-hub/db"
 
-func GenerateTemplateHub(conf map[string]int, filename string) {
+func GenerateTemplateHub(geo []string, geo_count []int, filename string) {
     hub := db.NewDataBase()
     hub.AddTemplate(0, "left_to_right", "", "left_to_right")
     
     var total int 
-    for geo, count := range conf {
-        for i := 0; i < count; i++ {
-            hub.AddGeometry(0, total, geo)
+    for i := range geo {
+        for j := 0; j < geo_count[i]; j++ {
+            hub.AddGeometry(0, total, geo[i])
             hub.AddAttr(0, total, "parent", "0")
             total++
         }
